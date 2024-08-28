cacce739b759da67e1c59f62563caaecd068cd68
SreenandhMt <152959595+SreenandhMt@users.noreply.github.com>
2024-08-28 23:14:44 +0530

    ✅ done - (AppBar and Scroll Control)

--- lib/config/config_pages.dart
+++ lib/config/config_pages.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio_website/connect/connect_page.dart';
 import 'package:portfolio_website/project/project_page.dart';
 
 import '../about/about_page.dart';
 import '../home/home_page.dart';
+
+ScrollController scrollController = ScrollController();
+const appBarSpace = SizedBox(width: 80);
+const appBarSpaceMobile = SizedBox(width: 30);
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
@@ -12,6 +17,8 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar:  PreferredSize(preferredSize: Size(size.width, 50), child: CustomAppbars()),
         body: Stack(
             children: [
               Container(
@@ -24,7 +31,8 @@ class MainPage extends StatelessWidget {
                       fit: BoxFit.cover)),
             ),
             ListView(
-              children: [
+              controller: scrollController,
+              children: const [
                 HomePage(),
                 AboutSection(),
                 ProjectShowcaseSection(),
@@ -33,6 +41,37 @@ class MainPage extends StatelessWidget {
             ),
           ],
         ),
+      );
+  }
+}
+
+class CustomAppbars extends StatelessWidget {
+  const CustomAppbars({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Expanded(child: SizedBox(height: 50)),
+          InkWell(mouseCursor: SystemMouseCursors.click,onTap: () {
+            scrollController.animateTo(0, duration: const Duration(seconds: 1), curve: Easing.legacy);
+          },child: Text("Home",style: GoogleFonts.inconsolata(fontSize: 17),)),
+          if(size.width>=1000) appBarSpace else appBarSpaceMobile,
+          InkWell(mouseCursor: SystemMouseCursors.click,onTap: () {
+            scrollController.animateTo(size.height*1.07, duration: const Duration(seconds: 1), curve: Easing.legacy);
+          },child: Text("About",style: GoogleFonts.inconsolata(fontSize: 17),)),
+          if(size.width>=1000) appBarSpace else appBarSpaceMobile,
+          InkWell(mouseCursor: SystemMouseCursors.click,onTap: () {
+            scrollController.animateTo(size.height*2.1, duration: const Duration(seconds: 1), curve: Easing.legacy);
+          },child: Text("Project",style: GoogleFonts.inconsolata(fontSize: 17),)),
+          if(size.width>=1000) appBarSpace else appBarSpaceMobile,
+          InkWell(mouseCursor: SystemMouseCursors.click,onTap: () {
+            scrollController.animateTo(size.height*2.1, duration: const Duration(seconds: 1), curve: Easing.legacy);
+          },child: Text("Connect Me",style: GoogleFonts.inconsolata(fontSize: 17),)),
+          if(size.width>=1000) appBarSpace else appBarSpaceMobile,
+        ],
       );
   }
 }
